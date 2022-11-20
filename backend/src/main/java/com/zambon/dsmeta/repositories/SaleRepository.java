c6d3a5197eb32fef952c086ede903c3012e21f27
grazzianiz <grazzianiz@gmail.com>
2022-11-20 13:06:08 -0300

    Date select

--- backend/src/main/java/com/zambon/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/zambon/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,15 @@
 package com.zambon.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zambon.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :_min AND :_max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate _min, LocalDate _max, Pageable _pageable);
 }
--- backend/src/main/java/com/zambon/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/zambon/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 	package com.zambon.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.zambon.dsmeta.entities.Sale;
@@ -14,7 +19,11 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String _minDate, String _maxDate, Pageable _pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate minDate = _minDate.equals("") ? today.minusYears(1) : LocalDate.parse(_minDate);
+		LocalDate maxDate = _maxDate.equals("") ? today : LocalDate.parse(_maxDate);
+		
+		return repository.findSales(minDate, maxDate, _pageable);
 	}
 }
